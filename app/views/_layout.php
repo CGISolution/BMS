c11a283adbff81d7d83703b4c43a30c8cf548153
William Gallios <williamgallios@gmail.com>
2014-08-23 18:42:29 -0400

    updates to framework

--- app/views/_layout.php
+++ app/views/_layout.php
@@ -3,7 +3,9 @@
   <head>
     <title>BMS</title>
 
+    <link rel='stylesheet' href='/public/css/main.css' type='text/css' />
     <script src='/vendor/components/angular.js/angular.js' type='text/javascript'></script>
+
   </head>
   <body>
     <?=$body?>
